17b51ac64177779959961da69f75ec7be9b76d34
Bird <40967626+SuperSirBird@users.noreply.github.com>
2018-12-22 21:20:45 +1100

    Create engine.js

---
+++ engine.js
@@ -0,0 +1,17 @@
+/* Engine js
+
+A Javascript Sonic Physics Engine
+
+@author: SuperSirBird (Github)
+@description: A sonic js physics engine
+@date: 22/12/2018 AUS
+@credit: Sonic Inspriation
+
+Feel free to use engine in other projects
+however credit would be appriciated
+
+*/
+
+/*
+Begin Code
+*/
