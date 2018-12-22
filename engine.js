3d9350161a399f72098a4d776067874a884a2251
Bird <40967626+SuperSirBird@users.noreply.github.com>
2018-12-23 10:47:22 +1100

    Update engine.js

--- engine.js
+++ engine.js
@@ -69,17 +69,17 @@ function linepoint(x1_,y1_,x2_,y2_,x_,y_) {
 
 function player() {
   linepoint(-500,-300,500,300,playerx,playery);
+  accelerate+=1;
+  if (accelerate>20) {accelerate=20}
+  playerx+=accelerate*Math.sin(Math.atan2(x-playerx,y-playery))
+  playery+=accelerate*Math.cos(Math.atan2(x-playerx,y-playery))
+  
+  // Check for obj's loop
   if (dist(x-playerx,y-playery)<playersize+linesize+20) {
     accelerate=0;
     playerx+=Math.sin(Math.atan2(x-playerx,y-playery))*-(playersize+linesize+20-dist(x-playerx,y-playery))
     playery+=Math.cos(Math.atan2(x-playerx,y-playery))*-(playersize+linesize+20-dist(x-playerx,y-playery))
     
-  }
-  if (dist(x-playerx,y-playery)>playersize+linesize+20) {
-    accelerate+=1;
-    if (accelerate>20) {accelerate=20}
-    playerx+=accelerate*Math.sin(Math.atan2(x-playerx,y-playery))
-    playery+=accelerate*Math.cos(Math.atan2(x-playerx,y-playery))
   }
   
   if (keys[32]) {accelerate = -8}
