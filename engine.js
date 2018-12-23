83db5d8b623a93320a1b900789656e2345c82191
Bird <40967626+SuperSirBird@users.noreply.github.com>
2018-12-23 12:09:35 +1100

    Update engine.js

--- engine.js
+++ engine.js
@@ -72,24 +72,24 @@ function player() {
   accelerate+=1;
   if (keys[32]) {accelerate = -8}
   // Check for obj's loop
-  if (dist(x-playerx,y-playery)>playersize+linesize+20) {
+  if (dist(x-playerx,y-playery)>playersize+linesize+10) {
     if (accelerate>20) {accelerate=20}
     playerx+=accelerate*Math.sin(Math.atan2(x-playerx,y-playery))
     playery+=accelerate*Math.cos(Math.atan2(x-playerx,y-playery))
   }
-  if (dist(x-playerx,y-playery)<playersize+linesize+20) {
+  if (dist(x-playerx,y-playery)<playersize+linesize+10) {
     accelerate=0;
     playerx+=Math.sin(Math.atan2(x-playerx,y-playery))*-2
     playery+=Math.cos(Math.atan2(x-playerx,y-playery))*-2
 
   }
   if (keys[39]) {
-    playerx+=Math.sin(90+Math.atan2(x-playerx,y-playery))*2
-    playery+=Math.cos(90+Math.atan2(x-playerx,y-playery))*2
+    playerx+=Math.sin(90+Math.atan2(x-playerx,y-playery))*-6
+    playery+=Math.cos(90+Math.atan2(x-playerx,y-playery))*-6
   }
   if (keys[37]) {
-    playerx+=Math.sin(-90+Math.atan2(x-playerx,y-playery))*2
-    playery+=Math.cos(-90+Math.atan2(x-playerx,y-playery))*2
+    playerx+=Math.sin(-90+Math.atan2(x-playerx,y-playery))*6
+    playery+=Math.cos(-90+Math.atan2(x-playerx,y-playery))*6
   }
 
 
