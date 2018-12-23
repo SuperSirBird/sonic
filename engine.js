8abe9a12ab46311ce465ab464e4c7364e0ec607d
Bird <40967626+SuperSirBird@users.noreply.github.com>
2018-12-23 14:05:36 +1100

    Update engine.js

--- engine.js
+++ engine.js
@@ -56,7 +56,7 @@ function gy(y_) {return ((y_*-1)+window.innerHeight/2)}
 
 function sonicsprite() {
   var img = document.getElementById("sonicwalk");
-  ctx.drawImage(img, gx(playerx), gy(playery),playersize,playersize);
+  ctx.drawImage(img, gx(playerx), gy(playery),playersize*2.5,playersize*2.5);
 }
 
 function linepoint(x1_,y1_,x2_,y2_,x_,y_) {
