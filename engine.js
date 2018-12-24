2afae37d3414d13e736d0955f2a3a4360a30d903
Bird <40967626+SuperSirBird@users.noreply.github.com>
2018-12-24 11:21:50 +1100

    Update engine.js

--- engine.js
+++ engine.js
@@ -126,37 +126,9 @@ function getclose() {
       smallestitem=i;
     }
   }
-  closeline=smallestitem;
-  
-  // If angle to sharp, revert
-  
-  linepoint(linex1[closeline],liney1[closeline],linex2[closeline],liney2[closeline],playerx,playery);
-  var rota2 = Math.atan2(x-playerx,y-playery);
-  var diff = 0;
-  if (Math.abs((Math.abs(rota*180/Math.PI))-(Math.abs(rota2*180/Math.PI))) < 180) {
-    diff = (Math.abs(rota*180/Math.PI))-(Math.abs(rota2*180/Math.PI));
-  } else {
-    diff = 180-((Math.abs(rota*180/Math.PI))-(Math.abs(rota2*180/Math.PI)));
-  }
-  if (Math.abs((Math.abs(rota*180/Math.PI))-(Math.abs(rota2*180/Math.PI))) == 180) {diff = 180}
-  
-  ctx.fillText(Math.abs(diff) + " rota1: " + rota*180/Math.PI + " rota2: " + rota2*180/Math.PI, 250, 50);
-  
-  if (Math.abs(diff) > 60) {
-    closeline = oldl;
-    playerx = olx;
-    playery = oly;
-    accelerate = olv;
-    
-  }
   
   // If switch reset velocity
   if (closeline != oldl) {accelerate=0}
-  
-  // If in air keep same
-  if (onground==0) {
-    closeline=oldl;
-  }
   
 }
 
