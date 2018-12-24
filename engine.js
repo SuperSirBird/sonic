83ca772cfbc8eccfccd79719edab68818f2d2b7d
Bird <40967626+SuperSirBird@users.noreply.github.com>
2018-12-24 16:56:28 +1100

    Update engine.js

--- engine.js
+++ engine.js
@@ -162,7 +162,7 @@ function player() {
   
   accelerate+=1;
   var jumpframe = false;
-  if (keys[32] && onground==1 && framecount-lastjump>20) {
+  if (keys[32] && onground==1 && framecount-lastjump>40) {
     lastjump = framecount;
     accelerate = -18;
     jumpframe = true;
