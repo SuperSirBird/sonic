f7dadf6c6817508085352eedbac9897858e4d87b
Bird <40967626+SuperSirBird@users.noreply.github.com>
2018-12-24 16:16:26 +1100

    Update engine.js

--- engine.js
+++ engine.js
@@ -96,10 +96,10 @@ function sonicsprite() {
   
   // Centering Sonics Position
   var transx = gx(playerx-((playersize*2.6)/2)*Math.sin(dir));
-  var transy = gy(playery-((playersize*yh)/2)*Math.cos(dir));
+  var transy = gy(playery-((playersize*2.6)/2)*Math.cos(dir));
   
   transx -= ((playersize*2.6)/2)*Math.sin(dir+(270*Math.PI/180));
-  transy -= ((playersize*yh)/2)*Math.cos(dir+(90*Math.PI/180));
+  transy -= ((playersize*2.6)/2)*Math.cos(dir+(90*Math.PI/180));
   ctx.translate(transx,transy);
   
   // Find Rotation
