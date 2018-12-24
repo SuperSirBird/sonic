493cef4807032ee399d82bacb32a468ac55b5076
Bird <40967626+SuperSirBird@users.noreply.github.com>
2018-12-24 12:54:39 +1100

    Update engine.js

--- engine.js
+++ engine.js
@@ -86,7 +86,7 @@ function sonicsprite() {
   }
   
   if (Math.abs(rota-oldir) < 180) {
-    dir=oldir + (rota-oldir)/2;
+    dir=oldir + ((rota-oldir) % 360)/2;
   } else {dir=oldir + ((180-rota-oldir) % 360)/2;}
   
   dir = oldir + (rota-oldir)/2;
